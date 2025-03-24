2f85e6149ad8fb78bbbfbec631c516add4e08f27
Alex Drankov <adrankov@schueco.com>
2025-03-24 11:14:24 +0100

    update version calculation script

--- calculateNuGETVersion.py
+++ calculateNuGETVersion.py
@@ -21,12 +21,15 @@ with open("nuget_version.json", "rt") as fIn:
 
     if not "type" in nuget_version or nuget_version["type"] == "":
         if stream == "main" or stream == "master":
-            nuget_version["type"] = "rc"
+            nuget_version["type"] = ""
         elif stream == "develop" or stream == "development":
             nuget_version["type"] = "beta"
         else:
             nuget_version["type"] = "alpha." + stream
     nuget_version["change"] = changelist
 
-    version = "%(major)s.%(minor)s.%(patch)s.%(change)s-%(type)s" % nuget_version
+    if stream == "main" or stream == "master":
+        version = "%(major)s.%(minor)s.%(patch)s" % nuget_version
+    else:
+        version = "%(major)s.%(minor)s.%(patch)s.%(type)s.%(change)s" % nuget_version
     print(version, end="")
